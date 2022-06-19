86e5114f12dc44b0ed30de59572d6e42e6ddca9e
Brian Maher <bmaher91@gmail.com>
2022-06-18 18:46:13 -0600

    play with simple smoothstep function

--- src/shaders/fragment-shader.glsl
+++ src/shaders/fragment-shader.glsl
@@ -6,6 +6,8 @@ void main() {
     vec3 red = vec3(1.0, 0.0, 0.0);
     vec3 blue = vec3(0.0, 0.0, 1.0);
 
-    color = mix(red, blue, vec3(vUv.x));
+    // color = vec3(smoothstep(0.0, 1.0, vUv.x));
+    color = mix(red, blue, smoothstep(0.0, 1.0, vUv.x));
+
     gl_FragColor = vec4(color, 1.0);
 }
