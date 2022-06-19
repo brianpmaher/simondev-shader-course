f858a0971b30d76b21d7869b0820476760261588
Brian Maher <bmaher91@gmail.com>
2022-06-18 19:02:58 -0600

    mix and smoothstep visualized

--- src/shaders/fragment-shader.glsl
+++ src/shaders/fragment-shader.glsl
@@ -3,11 +3,28 @@ varying vec2 vUv;
 void main() {
     vec3 color = vec3(0.0);
 
+    float value1 = vUv.x;
+    float value2 = smoothstep(0.0, 1.0, vUv.x);
+
+    float line = smoothstep(0.0, 0.005, abs(vUv.y - 0.5));
+    float linearLine = smoothstep(0.0, 0.0075, abs(vUv.y - mix(0.5, 1.0, value1)));
+    float smoothLine = smoothstep(0.0, 0.0075, abs(vUv.y - mix(0.0, 0.5, value2)));
+
     vec3 red = vec3(1.0, 0.0, 0.0);
     vec3 blue = vec3(0.0, 0.0, 1.0);
+    vec3 white = vec3(1.0, 1.0, 1.0);
 
-    // color = vec3(smoothstep(0.0, 1.0, vUv.x));
-    color = mix(red, blue, smoothstep(0.0, 1.0, vUv.x));
+    if(vUv.y > 0.5) {
+        color = mix(red, blue, vUv.x);
+    } else {
+        color = mix(red, blue, smoothstep(0.0, 1.0, vUv.x));
+    }
+
+    color = mix(white, color, line);
+    color = mix(white, color, linearLine);
+    color = mix(white, color, smoothLine);
+
+    // color = vec3(linearLine);
 
     gl_FragColor = vec4(color, 1.0);
 }
