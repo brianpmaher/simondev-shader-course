250aeebe1d5bd1d6f33c019167eecd830f301e21
Brian Maher <bmaher91@gmail.com>
2022-06-18 18:42:05 -0600

    play with mix gl func

--- src/shaders/fragment-shader.glsl
+++ src/shaders/fragment-shader.glsl
@@ -2,6 +2,10 @@ varying vec2 vUv;
 
 void main() {
     vec3 color = vec3(0.0);
-    color = vec3(step(0.25, vUv.x));
+
+    vec3 red = vec3(1.0, 0.0, 0.0);
+    vec3 blue = vec3(0.0, 0.0, 1.0);
+
+    color = mix(red, blue, vec3(vUv.x));
     gl_FragColor = vec4(color, 1.0);
 }
