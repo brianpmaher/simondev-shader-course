e2b086e57789fcfbcd3009ff56a8f911a4722d5b
Brian Maher <bmaher91@gmail.com>
2022-06-18 18:34:25 -0600

    tweak prettier config and rerun

--- src/main.ts
+++ src/main.ts
@@ -18,15 +18,21 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
-window.addEventListener('resize', () => renderer.setSize(window.innerWidth, window.innerHeight));
+window.addEventListener('resize', () =>
+  renderer.setSize(window.innerWidth, window.innerHeight),
+);
 
 const scene = new Scene();
 
 const camera = new OrthographicCamera(0, 1, 1, 0, 0.1, 1000);
 camera.position.set(0, 0, 1);
 
-const vertexShader = await fetch('src/shaders/vertex-shader.glsl').then((r) => r.text());
-const fragmentShader = await fetch('src/shaders/fragment-shader.glsl').then((r) => r.text());
+const vertexShader = await fetch('src/shaders/vertex-shader.glsl').then((r) =>
+  r.text(),
+);
+const fragmentShader = await fetch('src/shaders/fragment-shader.glsl').then(
+  (r) => r.text(),
+);
 
 const loader = new TextureLoader();
 const lakeTexture = loader.load('../assets/textures/hebgen-lake.jpg');
