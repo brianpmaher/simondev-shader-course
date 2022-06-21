45f17801d10a53ce663a8754d6adfdd625d44a55
Brian Maher <bmaher91@gmail.com>
2022-06-21 04:21:13 -0600

    make a grid with sin

--- src/shaders/fragment-shader.glsl
+++ src/shaders/fragment-shader.glsl
@@ -15,12 +15,12 @@ vec3 red = vec3(1.0, 0.0, 0.0);
 vec3 blue = vec3(0.0, 0.0, 1.0);
 
 void main() {
-    vec3 color = vec3(time);
+    vec3 color = vec3(0.0);
 
-    float t = sin(time);
-    t = remap(t, -1.0, 1.0, 0.0, 1.0);
+    float t1 = sin(vUv.x * 100.0);
+    float t2 = sin(vUv.y * 100.0);
 
-    color = mix(red, blue, t);
+    color = vec3(max(t1, t2));
 
     gl_FragColor = vec4(color, 1.0);
 }
