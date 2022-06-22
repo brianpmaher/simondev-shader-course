42ed129e897dd34ece20dc223a435e24579f2e84
Brian Maher <bmaher91@gmail.com>
2022-06-22 06:11:39 -0600

    add ambient lighting

--- src/shaders/fragment-shader.glsl
+++ src/shaders/fragment-shader.glsl
@@ -1,7 +1,15 @@
 varying vec2 vUv;
 
 void main() {
-    vec3 color = vec3(0.0);
+    vec3 baseColor = vec3(0.5);
+    vec3 lighting = vec3(0.0);
+
+    // Ambient
+    vec3 ambeint = vec3(0.5);
+
+    lighting = ambeint;
+
+    vec3 color = baseColor * lighting;
 
     gl_FragColor = vec4(color, 1.0);
 }
